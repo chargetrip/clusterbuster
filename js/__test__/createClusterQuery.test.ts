ef7eb81d3eedb528996dbfea2695e8ff026a0789
Bartol Karuza <bartol.k@gmail.com>
2019-10-23 18:24:00 +0200

    chore(tiler): fix tests and add CI build step

--- js/__test__/createClusterQuery.test.ts
+++ js/__test__/createClusterQuery.test.ts
@@ -1,33 +1,38 @@
-const {
-    createQueryForTile,
-    attributeMapToFeatureAttribute,
-} = require('../createClusterQuery');
+const { createQueryForTile } = require('../createClusterQuery');
 
 describe('createClusterQuery', () => {
-    it('should create an unclustered Query', () => {
-        expect(
-            createQueryForTile({
-                z: 1,
-                x: 0,
-                y: 1,
-                table: 'public.stations',
-                geometry: 'wkb_geometry',
-                maxZoomLevel: 10,
-                resolution: 512,
-                attributeMap: { a: 'b' },
-            })
-        ).toMatchSnapshot();
-    });
+  it('should create a clustered Query', () => {
+    expect(
+      createQueryForTile({
+        z: 1,
+        x: 0,
+        y: 1,
+        table: 'public.stations',
+        geometry: 'wkb_geometry',
+        maxZoomLevel: 10,
+        resolution: 512,
+        attributes: ['a'],
+        query: ['status = status', '[1, 2] @> [2, 3]'],
+      })
+    ).toMatchSnapshot();
+  });
 
-    it('should map attributes to features select array', () => {
-        expect(attributeMapToFeatureAttribute({
-            a: 'b',
-            c: 'd',
-            e: 'f',
-        })).toMatchSnapshot();
-    });
+  it('should create a unclustered Query', () => {
+    expect(
+      createQueryForTile({
+        z: 15,
+        x: 0,
+        y: 1,
+        table: 'public.stations',
+        geometry: 'wkb_geometry',
+        maxZoomLevel: 10,
+        resolution: 512,
+        attributes: ['a'],
+        query: [],
+      })
+    ).toMatchSnapshot();
+  });
 });
-
 // http://localhost:3005/stations/13/4400/2687/tile.mvt
 // http://localhost:3005/stations/14/8609/5642/tile.mvt
 // http://localhost:3005/stations/1/0/1/tile.mvt
