4937d972155ba0dfb46ed8c92d962080606bc0d7
Cosmin Petrescu <cosmin.petrescu@innobyte.com>
2019-10-24 18:12:00 +0300

    fix(tiler): SQL file path fix

--- src/utils/supporting.ts
+++ src/utils/supporting.ts
@@ -5,14 +5,14 @@ export default async function createSupportingSQLFunctions(pool) {
   console.log('attempting to create supporting SQL functions');
   try {
     await pool.query(
-      fs.readFileSync(path.join(__dirname, '../../sql/First.sql'), 'utf-8')
+      fs.readFileSync(path.join(__dirname, '../sql/First.sql'), 'utf-8')
     );
   } catch (e) {
     console.log('failure in creating First SQL function');
   }
   try {
     await pool.query(
-      fs.readFileSync(path.join(__dirname, '../../sql/TileBBox.sql'), 'utf-8')
+      fs.readFileSync(path.join(__dirname, '../sql/TileBBox.sql'), 'utf-8')
     );
   } catch (e) {
     console.log('failure in creating TileBBox SQL function');
@@ -20,7 +20,7 @@ export default async function createSupportingSQLFunctions(pool) {
   try {
     await pool.query(
       fs.readFileSync(
-        path.join(__dirname, '../../sql/TileDoubleBBox.sql'),
+        path.join(__dirname, '../sql/TileDoubleBBox.sql'),
         'utf-8'
       )
     );
