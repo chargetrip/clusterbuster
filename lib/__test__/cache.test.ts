98678a0bdf6cce2ec62be62e0a53d60994370036
Cosmin Petrescu <cosmin.petrescu@innobyte.com>
2019-10-29 11:45:50 +0200

    fix(tiller): Update cache test and snapshot

--- lib/__test__/cache.test.ts
+++ lib/__test__/cache.test.ts
@@ -1,7 +1,14 @@
-const { getCacheKey } = require('../cache');
+import { getCacheFiltersKey, getCacheKey } from '../cache';
 
 describe('getCacheKey', () => {
   it('should generate key', () => {
-    expect(getCacheKey('table', 0, 1, 2, ['1', '2'])).toMatchSnapshot();
+    expect(getCacheKey('table', 0, 1, 2)).toMatchSnapshot();
+  });
+});
+
+describe('getCacheFiltersKey', () => {
+  it('should generate key', () => {
+    expect(getCacheFiltersKey(['1', '2'])).toMatchSnapshot();
+    expect(getCacheFiltersKey(['2', '1'])).toMatchSnapshot();
   });
 });
