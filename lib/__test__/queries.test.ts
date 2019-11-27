9daa83e754ddcf850f959586b99cad5ecf67d58c
Bartol Karuza <bartol.k@gmail.com>
2019-11-27 15:38:07 +0000

    fix(tiler): update unit test and add new one for zoomToDistance

--- lib/__test__/queries.test.ts
+++ lib/__test__/queries.test.ts
@@ -1,3 +1,5 @@
+import { zoomToDistance as defaultZoomToDistance } from '../queries';
+
 const { createQueryForTile } = require('../queries');
 
 describe('createClusterQuery', () => {
@@ -14,6 +16,27 @@ describe('createClusterQuery', () => {
       bufferSize: 256,
       attributes: ['a'],
       query: ['status = status', '[1, 2] @> [2, 3]'],
+      zoomToDistance: defaultZoomToDistance,
+    });
+    expect(query.sql).toMatchSnapshot();
+    expect(query.values).toMatchSnapshot();
+  });
+
+  it('should create a clustered Query with differentZoomToDistance', () => {
+    const query = createQueryForTile({
+      z: 1,
+      x: 0,
+      y: 1,
+      table: 'public.stations',
+      geometry: 'wkb_geometry',
+      sourceLayer: 'points',
+      maxZoomLevel: 10,
+      radius: 2,
+      extent: 4096,
+      bufferSize: 256,
+      attributes: ['a'],
+      query: ['status = status', '[1, 2] @> [2, 3]'],
+      zoomToDistance: (zoomLevel, radius) => zoomLevel + radius,
     });
     expect(query.sql).toMatchSnapshot();
     expect(query.values).toMatchSnapshot();
@@ -33,6 +56,7 @@ describe('createClusterQuery', () => {
         bufferSize: 256,
         attributes: ['a'],
         query: [],
+        zoomToDistance: defaultZoomToDistance,
       })
     ).toMatchSnapshot();
   });
